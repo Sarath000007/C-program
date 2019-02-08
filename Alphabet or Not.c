218537194f2db2d4a311ce87515ca2f5653dbc4d
Sarath Chander <47447963+sarathchander08@users.noreply.github.com>
2019-02-08 19:12:45 +0530

    Create Alphabet or Not.c

---
+++ Alphabet or Not.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	char c;
+	scanf("%s",&c);
+	if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
+	{
+		printf("Alphabet");
+	}
+	else
+	{
+		printf("No");
+	}
+	return 0;
+}
